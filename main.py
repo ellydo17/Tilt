dde75f5907ca1086e2cb19eef24dfe6e52150af6
Elly Do <nganthanhdo.00@gmail.com>
2021-11-20 18:52:15 -0500

    Updating tilting up & down

--- main.py
+++ main.py
@@ -22,11 +22,17 @@ for i in range(1, 37):
 #                   ["-", "-", "-", "-", "-"],
 #                   ["G", "B", "-", "-", "-"]])
 
-board = np.array([["-", "B", "I", "-", "G"],
-                  ["-", "-", "-", "-", "-"],
-                  ["-", "-", "X", "-", "G"],
-                  ["-", "-", "-", "-", "-"],
-                  ["-", "-", "-", "B", "G"]])
+# board = np.array([["-", "B", "I", "-", "G"],
+#                   ["-", "-", "-", "-", "-"],
+#                   ["-", "-", "X", "-", "G"],
+#                   ["-", "-", "-", "-", "-"],
+#                   ["-", "-", "-", "B", "G"]])
+
+board = np.array([["G", "-", "G", "-", "G"],
+                  ["-", "-", "-", "-", "B"],
+                  ["I", "-", "X", "-", "-"],
+                  ["B", "-", "-", "-", "-"],
+                  ["-", "-", "-", "-", "-"]])
 
 tempBoard = board.copy()
 
@@ -99,6 +105,73 @@ for d in directions:
             if stop == True:
                 break
         # print(board)
+
+    # tilt down
+    if d == "D":
+        stop = False
+        for i in range(len(board)):
+            column = board[:, i]
+            index = len(column)
+            blocker = index
+            hole = index // 2 + 1
+            for j in range(len(column) - 1, -1, -1):
+                elem = column[j]
+                if i == hole - 1:
+                    if elem == "B" and j < hole - 1 < blocker:
+                        stop = True
+                        board = tempBoard
+                        break
+                    elif elem == "G" and (j == hole - 2 or j == hole - 3):
+                        column[j] = "-"
+                        continue
+                if elem == "-" and j == len(column) - 1:
+                    index = j
+                elif elem == "-" and column[j + 1] != "-":
+                    index = j
+                else:
+                    if (elem == "B" or elem == "G") and (blocker > index):
+                        column[index] = elem
+                        column[j] = "-"
+                        index -= 1
+                    elif elem == "I":
+                        blocker = j
+            if stop == True:
+                break
+        # print(board)
+
+    # tilt up
+    if d == "U":
+        stop = False
+        for i in range(len(board)):
+            # loop through each row in the board
+            column = board[:, i]
+            index = -1
+            blocker = index
+            hole = len(column) // 2 + 1
+            for j in range(len(column)):
+                elem = column[j]
+                if i == hole - 1:
+                    if elem == "B" and j > hole - 1 > blocker:
+                        stop = True
+                        board = tempBoard
+                        break
+                    elif elem == "G" and (j == len(column) or j == len(column) - 1):
+                        column[j] = "-"
+                        continue
+                if elem == "-" and j == 0:
+                    index = j
+                elif elem == "-" and column[j - 1] != "-":
+                    index = j
+                else:
+                    if (elem == "B" or elem == "G") and (blocker < index):
+                        column[index] = elem
+                        column[j] = "-"
+                        index += 1
+                    elif elem == "I":
+                        blocker = j
+            if stop == True:
+                break
+        print(board)
 
 # g.show('intermediate.html')
 
